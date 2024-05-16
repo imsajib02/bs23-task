f2c22903b5a2c21d3adec79422ef4d14ca65d072
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-16 18:00:15 +0600

    Home page view added

--- lib/pages/home/view/home.dart
+++ lib/pages/home/view/home.dart
@@ -1,20 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+import '../../../barrels/widgets.dart';
+import '../controller/home_controller.dart';
 
 class Home extends StatelessWidget {
+
+  final _homeController = Get.put(HomeController());
 
   Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+    return WillPopScope(
+      onWillPop: () {
+        SystemNavigator.pop();
+        return Future(() => false);
+      },
+      child: Scaffold(
+        body: Builder(
+          builder: (context) {
 
+            return SafeArea(
+              child: Obx(() => IndexedStack(
+                index: _homeController.index.value,
+                children: [
 
-          ],
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+
+                  Column(
+                    children: [],
+                  ),
+
+                  ErrorPage(
+                    onTap: () => _homeController.retry(),
+                  ),
+                ],
+              )),
+            );
+          },
         ),
       ),
     );
