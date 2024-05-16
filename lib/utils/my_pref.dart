587f7671384c29107ac2c4c9df6f335977392f75
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-16 13:12:41 +0600

    Load storage pref name from environment

--- lib/utils/my_pref.dart
+++ lib/utils/my_pref.dart
@@ -1,10 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_storage/get_storage.dart';
 
 import '../barrels/localizations.dart';
 
 class MyPref {
 
-  final _box = GetStorage('MyStoragePref');
+  final _box = GetStorage(dotenv.env['STORAGE_PREF']!);
 
   static const LANGUAGE = 'language';
 
