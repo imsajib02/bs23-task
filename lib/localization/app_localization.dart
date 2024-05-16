07a802d1977e44856a038ff50231ada4b66388d9
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-16 17:59:49 +0600

    New localization values added

--- lib/localization/app_localization.dart
+++ lib/localization/app_localization.dart
@@ -7,9 +7,23 @@ class AppLocalization extends Translations {
 
     'en_US': {
       'app_name': 'BS23 Task',
+      'oops': 'Oopss!',
+      'data_failure': 'Failed to get data!',
+      'try_again': 'Try Again',
+      'no_internet': 'No internet connection!',
+      'inactive_connection': 'Internet connection not working!',
+      'timed_out': 'Timed out!',
+      'STR_UNKNOWN_ERROR': 'Something went wrong!',
     },
     'bn_BD': {
       'app_name': 'বিএস২৩ টাস্ক',
+      'oops': 'Oopss!',
+      'data_failure': 'Failed to get data!',
+      'try_again': 'Try Again',
+      'no_internet': 'No internet connection!',
+      'inactive_connection': 'Internet connection not working!',
+      'timed_out': 'Timed out!',
+      'STR_UNKNOWN_ERROR': 'Something went wrong!',
     }
   };
 }
