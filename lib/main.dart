1efb0457e0043f21eec14751f460e293af542fb9
imsajib02 <shafiul.imsajib@gmail.com>
2024-05-16 13:14:55 +0600

    Storage pref and localization initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,46 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'barrels/localizations.dart';
+import 'barrels/utils.dart';
+import 'home.dart';
+
+Future<void> main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: kDebugMode ? '.env.development' : '.env.production');
+  await GetStorage.init(dotenv.env['STORAGE_PREF']!);
+
+  registerServices();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
+    runApp(MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
 
-  const MyApp({Key? key}) : super(key: key);
+  final localizationController = Get.put(LocalizationController());
+
+  MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Scaffold(),
-    );
+    return Obx(() => GetMaterialApp(
+      title: dotenv.env['APP_TITLE']!,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      defaultTransition: Transition.native,
+      translations: AppLocalization(),
+      locale: localizationController.appLocale.value,
+      fallbackLocale: getLocale(ENGLISH),
+      home: Home(),
+    ));
   }
 }
