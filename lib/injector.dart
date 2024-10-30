ea716c8aceb218dc5f5fe4d8695dd401e4148c2a
imsajib02 <shafiul.imsajib@gmail.com>
2024-10-30 13:35:10 +0600

    Dependency regster for repository, usecase, datasource

--- lib/injector.dart
+++ lib/injector.dart
@@ -5,6 +5,11 @@ import '/core/network/interceptor_barrel.dart';
 import '/core/routes/routes.dart';
 import '/core/storage/get_storage_manager.dart';
 import '/core/utils/error_parser.dart';
+import 'core/network/network_client.dart';
+import 'data/datasources/remote/remote_data_source_barrel.dart';
+import 'data/repositories/repository_impl_barrel.dart';
+import 'domain/repositories/repository_barrel.dart';
+import 'domain/usecases/use_case_barrel.dart';
 
 final injector = GetIt.instance;
 
@@ -26,5 +31,14 @@ Future<void> registerDependencies() async {
 
     // Network
     ..registerLazySingleton<NetworkInfo>(() => ConnectivityStatus())
-    ..registerLazySingleton<ErrorParser>(() => ApiResponseError());
+    ..registerLazySingleton<ErrorParser>(() => ApiResponseError())
+
+    // Data Sources
+    ..registerLazySingleton<RepoListRemoteDataSource>(() => RepoListRemoteDataSourceImpl(NetworkClient(injector(), [injector<AcceptTypeInterceptor>(), injector<LoggerInterceptor>(), injector<ResponseInterceptor>()])))
+
+    // Repositories
+    ..registerLazySingleton<RepoListRepository>(() => RepoListRepositoryImpl(injector()))
+
+    // Use Cases
+    ..registerLazySingleton<RepoListUseCase>(() => RepoListUseCase(injector()));
 }
--- lib/utils/custom_extensions.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension ExtSizedBox on int {
-  SizedBox get h => SizedBox(height: toDouble());
-  SizedBox get w => SizedBox(width: toDouble());
-}
