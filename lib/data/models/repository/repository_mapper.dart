557e1651c718017837a7bf2f99c420c6209433b9
imsajib02 <shafiul.imsajib@gmail.com>
2024-10-31 11:01:50 +0600

    New properties added

--- lib/data/models/repository/repository_mapper.dart
+++ lib/data/models/repository/repository_mapper.dart
@@ -1,18 +1,23 @@
 part of 'repository_model.dart';
 
 RepositoryModel _$fromJson(Map<String, dynamic> json) => RepositoryModel(
+  id: json['id'] as int?,
   name: json['name'] as String?,
   owner: json['owner']['login'] as String?,
   htmlUrl: json['html_url'] as String?,
   stars: json['stargazers_count'] as int?,
   forks: json['forks_count'] as int?,
+  openIssuesCount: json['open_issues_count'] as int?,
   description: json['description'] as String?,
   language: json['language'] as String?,
+  visibility: json['visibility'] as String?,
+  topics: json['topics'] == null ? null : List<String>.from(json['topics'].map((item) => item.toString())),
   updatedAt: json['updated_at'] as String?,
 );
 
 @override
 Map<String, dynamic> _$toJson(RepositoryModel model) => {
+  'id': model.id,
   'name': model.name,
   'owner': {
     'login': model.owner,
@@ -20,7 +25,10 @@ Map<String, dynamic> _$toJson(RepositoryModel model) => {
   'html_url': model.htmlUrl,
   'stargazers_count': model.stars,
   'forks_count': model.forks,
+  'open_issues_count': model.openIssuesCount,
   'description': model.description,
   'language': model.language,
+  'visibility': model.visibility,
+  'topics': model.topics?.map((topic) => topic),
   'updated_at': model.updatedAt,
 };
--- lib/data/models/repository/repository_model.dart
+++ lib/data/models/repository/repository_model.dart
@@ -5,30 +5,38 @@ part 'repository_mapper.dart';
 
 class RepositoryModel implements EntityConvertible<RepositoryModel, RepositoryEntity> {
 
+  final int? id;
   final String? name;
   final String? owner;
   final String? htmlUrl;
   final int? stars;
   final int? forks;
+  final int? openIssuesCount;
   final String? description;
   final String? language;
+  final String? visibility;
+  final List<String>? topics;
   final String? updatedAt;
 
-  const RepositoryModel({this.name, this.owner, this.htmlUrl, this.stars, this.forks,
-    this.language, this.description, this.updatedAt});
+  const RepositoryModel({this.id, this.name, this.owner, this.htmlUrl, this.stars, this.forks, this.openIssuesCount,
+    this.language, this.visibility, this.topics, this.description, this.updatedAt});
 
   @override
   RepositoryModel fromEntity(RepositoryEntity entity) => throw UnimplementedError();
 
   @override
   RepositoryEntity toEntity() => RepositoryEntity(
+    id: id,
     name: name,
     owner: owner,
     htmlUrl: htmlUrl,
     stars: stars,
     forks: forks,
+    openIssuesCount: openIssuesCount,
     description: description,
     language: language,
+    visibility: visibility,
+    topics: topics,
     updatedAt: updatedAt,
   );
 
