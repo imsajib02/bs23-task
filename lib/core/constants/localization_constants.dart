ef7c038c476f789d84e6442b0cf194b1d17bc993
imsajib02 <shafiul.imsajib@gmail.com>
2024-10-31 09:26:28 +0600

    Localization constants

---
+++ lib/core/constants/localization_constants.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const Locale localeEnglish = Locale('en', 'US');
+const Locale localeBangla = Locale('bn', 'BD');
+
+const List<({String name, String shortName, Locale locale})> appLocales = [
+  (name: 'বাংলা', shortName: 'বাং', locale: localeBangla),
+  (name: 'English', shortName: 'En', locale: localeEnglish),
+];
+
+const strAppName = 'app_name';
+const strFlutterRepos = 'flutter_repos';
+const strName = 'name';
+const strStar = 'star';
+const strOwner = 'owner';
+const strNoDescription = 'no_description';
+const strNun = 'nun';
+const strLastUpdate = 'last_update';
+const strNoInternet = 'no_internet';
+const strInactiveConnection = 'inactive_connection';
+const strTimedOut = 'timed_out';
+const strSomethingWrong = 'something_wrong';
+const strUnknownError = 'unknown_error';
+const strSuccess = 'success';
+const strFailed = 'failed';
+const strTryAgain = 'try_again';
+const strOops = 'oops';
+const strDataLoadFailure = 'data_load_failure';
+const strNoData = 'no_data_found';
