a656d2a87dd7aeab1b3c396d6b5bb2a03e8249e4
imsajib02 <shafiul.imsajib@gmail.com>
2024-10-31 09:26:42 +0600

    Color constants

---
+++ lib/core/constants/colors_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xFF0181cc);
+const Color accent = Color(0xFF039be6);
+
+const Color lightBackground = Color(0xFFF5F5F5);
+const Color darkBackground = Color(0xFF0f0f0f);
+
+const Color white = Color(0xFFFFFFFF);
+const Color amber = Color(0xFFF1B91B);
+const Color black = Color(0xFF000000);
+const Color grey = Color(0xFFA49F9F);
